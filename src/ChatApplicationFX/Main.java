1adb1df4d26a776526ec47efb1c1f5d7b8ce8773
Buket <buket-meltem.ozkan@epitech.eu>
2026-02-20 21:26:10 +0100

    feat(ui): add login form with username/password fields

--- src/ChatApplicationFX/Main.java
+++ src/ChatApplicationFX/Main.java
@@ -1,19 +1,44 @@
 package ChatApplicationFX;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("ChatApplicationFX.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 300));
+        GridPane gridPane = new GridPane();
+        Text text_login = new Text(10, 20, "Login");
+        TextField textField = new TextField();
+        Text text_password = new Text(10, 60, "Password");
+        PasswordField password = new PasswordField();
+        Button button = new Button("Login");
+        
+        gridPane.add(text_login, 0, 0);
+        gridPane.add(textField, 0, 1);
+        gridPane.add(text_password, 0, 2);
+        gridPane.add(password, 0, 3);
+        gridPane.add(button, 0, 4);
+        
+        primaryStage.setScene(new Scene(gridPane, 400, 300));
         primaryStage.show();
+
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String login = textField.getText();
+                String pass = password.getText();
+                System.out.println("Login: " + login + ", Password: " + pass); //Print login and password to console for testing
+            }
+        });
     }
 
 
